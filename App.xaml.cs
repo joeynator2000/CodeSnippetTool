355936442706278641e919e5fca9af5a58d5b0e7
John Smith <iandonker12@hotmail.com>
2021-06-01 14:08:56 +0200

    Working Main Class

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CodeSnippetTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            //DbManager db = new DbManager();
+
+            Db.DbConnect connect = new Db.DbConnect();
+
+            connect.testInsert();
+        }
+    }
+}
