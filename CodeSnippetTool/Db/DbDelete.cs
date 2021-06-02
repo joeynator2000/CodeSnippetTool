0dc0aca371b3bb93d3738e71b9ba905a25dc590c
Marek <marek.stryjenski@gmail.com>
2021-06-02 14:19:56 +0200

    Revert "Update DbDelete.cs"
    This reverts commit 5788aa9cd6ee9374fb86883ac643f0aa71f77dbc.

--- CodeSnippetTool/Db/DbDelete.cs
+++ CodeSnippetTool/Db/DbDelete.cs
@@ -1,29 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using MySql.Data.MySqlClient;
 
 namespace CodeSnippetTool.Db
 {
     class DbDelete
     {
-        public DbDelete()
-        {
-        }
-
-        //TODO Implement button click listener. Wrap function in a try catch block
-        public void DeleteSnippet(MySqlConnection mySqlConnection)
-        {
-            mySqlConnection.Open();
-            int id = 1;
-            string deleteQuery = "DELETE FROM snippets WHERE id='" + id + "';";
-            MySqlCommand delete = new MySqlCommand(deleteQuery, mySqlConnection);
-            MySqlDataReader reader;
-            reader = delete.ExecuteReader();
-            while(reader.Read())
-            {
-            }
-            mySqlConnection.Close();
-        }
     }
 }
