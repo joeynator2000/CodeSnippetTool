6b22766b16e9a751307f2995213c4b4217ed094b
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2021-06-08 12:19:50 +0200

    Updte on DBUpdte class: 1. changed code lyaout 2. added using instead of If to use connection

--- CodeSnippetTool/Db/DbUpdate.cs
+++ CodeSnippetTool/Db/DbUpdate.cs
@@ -8,26 +8,31 @@ namespace CodeSnippetTool.Db
 {
     class DbUpdate
     {
-        public DbUpdate{
+        public DbUpdate()
+        {
+
+        }
+
+        public void UpdateSnippet(int id, String snippet_text, String lang, String favourite, String date_added, String last_copied, MySqlConnection db)
+        {
+            //db.Open();
+            string updateQuery = "UPDATE snippets SET id=@id,snippet_text=@snippet_text,lang=@language,favourite=@favourite,date_added=@date_added,last_copied=@last_copied WHERE id=@id";
+            using (db)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = updateQuery;
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@snippet_text", snippet_text);
+                cmd.Parameters.AddWithValue("@language", lang);
+                cmd.Parameters.AddWithValue("@favourite", favourite);
+                cmd.Parameters.AddWithValue("@date_added", date_added);
+                cmd.Parameters.AddWithValue("@last_copied", last_copied);
+                cmd.ExecuteNonQuery();
+                db.Close();
+                //Bind();
+            }
+
+        }
 
     }
-    public void UpdateSnippet(int id, String snippet_text, String lang, String favourite, String date_added, String last_copied)
-        {
-            mySqlConnection.Open();
-            string updateQuery = "UPDATE snippets SET id=@id,snippet_text=@snippet_text,lang=@language,favourite=@favourite,date_added=@date_added,last_copied=@last_copied WHERE id=@id";
-            if (this.OpenConnection() == true)
-        {
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = updateQuery;
-            cmd.Parameters.AddWithValue("@id",id);
-            cmd.Parameters.AddWithValue("@snippet_text",snippet_text);
-            cmd.Parameters.AddWithValue("@language",lang);
-            cmd.Parameters.AddWithValue("@favourite",favourite);
-            cmd.Parameters.AddWithValue("@date_added",date_added);
-            cmd.Parameters.AddWithValue("@last_copied",last_copied);
-            cmd.ExecuteNonQuery();
-            this.CloseConnection();
-            Bind();
-        }
-        }
 }
