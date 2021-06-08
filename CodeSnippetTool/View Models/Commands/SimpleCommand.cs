44f9fdfd7ba581572c46dafc9befe50c8bcadb6b
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2021-06-08 13:14:38 +0200

    Added button command logic 1. added SimpleCommand demo class 2. adaded ViewModelBase class 3. added reference to these on button on MainWindow.xaml

---
+++ CodeSnippetTool/View Models/Commands/SimpleCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CodeSnippetTool.View_Models.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+        //end these files with command at the end
+        public event EventHandler CanExecuteChanged;
+
+        public ViewModelBase ViewModel { get; set; }
+
+        public SimpleCommand(ViewModelBase viewModel) 
+        {
+            this.ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //action here
+            this.ViewModel.SimpleMethod();
+        }
+    }
+}
