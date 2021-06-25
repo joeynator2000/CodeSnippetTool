b6125264f0a3de4a5499c86c588d0a7b68ac3725
spronchev <58593274+spronchev@users.noreply.github.com>
2021-06-25 19:33:25 +0300

    Db update check if hotkey and name is taken

--- CodeSnippetTool/Db/DbUpdate.cs
+++ CodeSnippetTool/Db/DbUpdate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using MySql.Data.MySqlClient;
 
 
@@ -18,21 +19,35 @@ namespace CodeSnippetTool.Db
 
         public void UpdateSnippet(int id, String name, String snippet_text, String lang, int favourite, string description, string hotKey, string dateAdded, string lastCopied)
         {
-           
-            string updateQuery = "UPDATE snippets SET id=@id, name=@name, snippet_text=@snippet_text,lang=@language,favourite=@favourite, description=@description, HotKey=@hotKey, date_added=@dateAdded, last_copied=@lastCopied WHERE id=@id";
-            MySqlCommand cmd = new MySqlCommand(updateQuery, db.databaseConnection);
-                cmd.Parameters.AddWithValue("@id", id);
-                cmd.Parameters.AddWithValue("@name", name);
-                cmd.Parameters.AddWithValue("@snippet_text", snippet_text);
-                cmd.Parameters.AddWithValue("@language", lang);
-                cmd.Parameters.AddWithValue("@favourite", favourite);
-                cmd.Parameters.AddWithValue("@description", description);
-                cmd.Parameters.AddWithValue("@HotKey", hotKey);
-                cmd.Parameters.AddWithValue("@date_added", dateAdded);
-                cmd.Parameters.AddWithValue("@last_copied", lastCopied);
-                db.getConnection().Open();
-                cmd.ExecuteNonQuery();
-                db.getConnection().Close();
+            if(!dbSelect.hotKeyIsTaken(hotKey))
+            {
+                if(!dbSelect.NameIsTaken(name))
+                {
+                    string updateQuery = "UPDATE snippets SET id=@id, name=@name, snippet_text=@snippet_text,lang=@language,favourite=@favourite, description=@description, HotKey=@hotKey, date_added=@dateAdded, last_copied=@lastCopied WHERE id=@id";
+                    MySqlCommand cmd = new MySqlCommand(updateQuery, db.databaseConnection);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@snippet_text", snippet_text);
+                    cmd.Parameters.AddWithValue("@language", lang);
+                    cmd.Parameters.AddWithValue("@favourite", favourite);
+                    cmd.Parameters.AddWithValue("@description", description);
+                    cmd.Parameters.AddWithValue("@HotKey", hotKey);
+                    cmd.Parameters.AddWithValue("@date_added", dateAdded);
+                    cmd.Parameters.AddWithValue("@last_copied", lastCopied);
+                    db.getConnection().Open();
+                    cmd.ExecuteNonQuery();
+                    db.getConnection().Close();
+                }
+                else
+                {
+                    MessageBox.Show("Name is already taken");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hotkey is already taken");
+            }
+        
         }
 
 
